a63567daf8150a8d34b890171e71864eb403a085
SeuCaiOo <caiopimentel8@gmail.com>
2018-12-18 20:51:57 -0200

    09.2 Completed

---
+++ DroidCafeWithSettings/app/src/main/java/com/example/android/droidcafeinput/OrderActivity.java
@@ -0,0 +1,62 @@
+package com.example.android.droidcafeinput;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OrderActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        mTextView = findViewById(R.id.order_textview);
+
+        Intent intent = getIntent();
+
+        if (intent != null) {
+            String order = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            mTextView.setText("Not found order !");
+
+            if (!order.equals("")) {
+                mTextView.setText(order);
+            }
+
+
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
